5b6fb3afcdee291b7142e71514f984b03df5bc92
Nathan Moore <ncmoore1986@gmail.com>
2017-10-17 13:44:33 -0700

    added the third function and fixed the gitignore

--- series.py
+++ series.py
@@ -1,21 +1,30 @@
-"""change me later."""
+"""Functions to return the nth value of various series."""
 
+if __name__ == "__main__":
 
-def fibonacci(n):
-    """Generate Fibonacci numbers up to the nth one, and then returns it."""
-    numbers = [0, 1]
+    def fibonacci(n):
+        """Generate Fibonacci numbers, then return the nth one."""
+        numbers = [0, 1]
 
-    for i in range(n - 2):
-        numbers.append((numbers[i]) + (numbers[i + 1]))
+        for i in range(n - 2):
+            numbers.append((numbers[i]) + (numbers[i + 1]))
 
-    return numbers[len(numbers - 1)]
+        return numbers[len(numbers - 1)]
 
+    def lucas(n):
+        """Generate Lucas numbers, then return the nth one."""
+        numbers = [2, 1]
 
-def lucas(n):
-    """Generate Lucas numbers up to the nth one, and then returns it."""
-    numbers = [2, 1]
+        for i in range(n - 2):
+            numbers.append((numbers[i]) + (numbers[i + 1]))
 
-    for i in range(n - 2):
-        numbers.append((numbers[i]) + (numbers[i + 1]))
+        return numbers[len(numbers - 1)]
 
-    return numbers[len(numbers - 1)]
+    def sum_series(n, first_num=0, second_num=1):
+        """Generate an arbitrary sum series, then return the nth value."""
+        numbers = [first_num, second_num]
+
+        for i in range(n - 2):
+            numbers.append((numbers[i]) + (numbers[i + 1]))
+
+        return numbers[len(numbers - 1)]
