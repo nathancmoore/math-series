9ecd762ea05a7523c516f5cb23c21ded1e1577f4
Nathan Moore <ncmoore1986@gmail.com>
2017-10-19 10:25:00 -0700

    added tests for valid inputs and corrected the output of the functions to allow for n=1

--- test_series.py
+++ test_series.py
@@ -1,14 +1,28 @@
+"""Test various functions of sum series."""
+
+
 import pytest
+
+
+@pytest.mark.parametrize('n', [0, "str", 4.6, [4, 5, 6], (1, 2, 3)])
+def test_entry_validity(n):
+    """Test for invalid entries."""
+    from series import fibonacci, lucas, sum_series
+    assert fibonacci(n) == 'Only integers above zero are valid!'
+    assert lucas(n) == 'Only integers above zero are valid!'
+    assert sum_series(n) == 'Only integers above zero are valid!'
 
 
 @pytest.mark.parametrize('n, result', [(3, 1), (5, 3), (8, 13)])
 def test_fibonacci(n, result):
+    """Test for numbers in the Fibonacci Sequence."""
     from series import fibonacci
     assert fibonacci(n) == result
 
 
 @pytest.mark.parametrize('n, result', [(4, 4), (6, 11), (8, 29)])
 def test_lucas(n, result):
+    """Test for numbers in the Lucas Sequence."""
     from series import lucas
     assert lucas(n) == result
 
@@ -22,5 +36,6 @@ SUM_TABLE = [
 
 @pytest.mark.parametrize('n, first_num, second_num, result', SUM_TABLE)
 def test_sum_series(n, first_num, second_num, result):
+    """Test for numbers in an arbitrary sum sequence."""
     from series import sum_series
     assert sum_series(n, first_num, second_num) == result
